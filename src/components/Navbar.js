da994bc3880b43c5d2bb5e561fa31560074f8118
Harsh_Kumar <vermah646@gmail.com>
2023-06-29 03:41:18 +0530

    Navbar updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,29 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom';
+import './Navbar.css';
+import { Button } from './Button';
 
 function Navbar() {
   const [click, setClick] = useState(false);
-  
+  const [button, setButton] = useState(true);
+
   const handleClick = ()=> setClick(!click);
   const closeMobileMenu = ()=> setClick(false);
   
+  const showButton = () => 
+  { 
+    if (window.innerWidth <= 960){ setButton(false); 
+    } else {
+        setButton(true);
+    }
+   };
+
+   useEffect(()=>{
+    showButton()
+   }, []);
+
+   window.addEventListener('resize', showButton);
+
   return (
    <>
     <nav className="navbar">
@@ -54,6 +71,7 @@ function Navbar() {
                     </Link>
                 </li>
             </ul>
+            {button && <Button buttonStyle='btn--outline'>Register Now</Button>}
         </div>
     </nav>
    </>
