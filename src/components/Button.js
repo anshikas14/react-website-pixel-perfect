c74a5818151d8a769ba62ae1713eb908d0062e2d
Harsh_Kumar <vermah646@gmail.com>
2023-06-30 05:01:00 +0530

    Third Commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -18,7 +18,7 @@ export const Button =({
   const checkButtonSize =SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
   return(
-    <Link to='/contact-us' className='btn-mobile'>
+    <Link to='/registration' className='btn-mobile'>
         <button
         className={'btn ${checkButtonStyle} ${checkButtonSize}'}
         >
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,46 +28,44 @@ function Navbar() {
    <>
     <nav className="navbar">
         <div className="navbar-container">
-            <Link to="/" className="navbar-logo">
-                WREC <i class="fa-sharp fa-solid fa-book-open-reader"></i>
-            </Link>
+        
             <div className="menu-icon" onClick={handleClick}>
                 <i className={click ? 'fas fa-times' : 'fas fa-bars' } />
             </div>
             <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                 <li className="nav-item">
-                    <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                        Home 
+                    <Link to='/home' className='nav-links' onClick={closeMobileMenu}>
+                    <i class="fa-sharp fa-solid fa-house"></i>
                     </Link>
                 </li>
                 <li className="nav-item">
                     <Link to='/speakers' className='nav-links' onClick={closeMobileMenu}>
-                        Speakers 
+                    <i class="fa-sharp fa-solid fa-microphone"></i>&nbsp;Speakers 
                     </Link>
                 </li>
                 <li className="nav-item">
                     <Link to='/committee' className='nav-links' onClick={closeMobileMenu}>
-                        Committee
+                    <i class="fa-sharp fa-solid fa-users-line"></i>&nbsp;Committee
                     </Link>
                 </li>
                 <li className="nav-item">
                     <Link to='/sponsors' className='nav-links' onClick={closeMobileMenu}>
-                        Sponsors
+                    <i class="fa-solid fa-indian-rupee-sign"></i>&nbsp;Sponsors
                     </Link>
                 </li>
                 <li className="nav-item">
                     <Link to='/awards' className='nav-links' onClick={closeMobileMenu}>
-                        Awards
+                    <i class="fa-sharp fa-solid fa-award"></i>&nbsp;Awards
                     </Link>
                 </li>
                 <li className="nav-item">
                     <Link to='/registration' className='nav-links' onClick={closeMobileMenu}>
-                        Registration
+                    <i class="fa-sharp fa-solid fa-id-card"></i>&nbsp;Registration
                     </Link>
                 </li>
                 <li className="nav-item">
                     <Link to='/contact-us' className='nav-links' onClick={closeMobileMenu}>
-                        Contact Us
+                    <i class="fa-sharp fa-solid fa-phone"></i>&nbsp; Contact Us
                     </Link>
                 </li>
             </ul>
