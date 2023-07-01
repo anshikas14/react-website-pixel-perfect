03e7cac0de8dba48acf24c67233eba7d9665be0c
Anshika <anshika140203@gmail.com>
2023-07-01 13:27:06 +0530

    changes to about us

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -34,11 +34,10 @@ function Footer() {
             <Link to='/'>Sponsors</Link>
           </div>
           <div class='footer-link-items'>
-            <h2>Contact Us</h2>
-            <Link to='/'>Contact</Link>
-            <Link to='/'>Support</Link>
-            <Link to='/'>Destinations</Link>
-            <Link to='/'>Sponsorships</Link>
+            <h2>Speakers</h2>
+            <Link to='/'>Keynote Speakers</Link>
+            <Link to='/'>Speakers for tutorials</Link>
+            <Link to='/'>Industry panel sessions</Link>
           </div>
         </div>
         <div className='footer-link-wrapper'>
@@ -49,33 +48,20 @@ function Footer() {
             <Link to='/'>Agency</Link>
             <Link to='/'>Influencer</Link>
           </div>
-          <div class='footer-link-items'>
-            <h2>Social Media</h2>
-            <Link to='/'>Instagram</Link>
-            <Link to='/'>Facebook</Link>
-            <Link to='/'>Youtube</Link>
-            <Link to='/'>Twitter</Link>
-          </div>
+    
+    
         </div>
       </div>
       <section class='social-media'>
         <div class='social-media-wrap'>
           <div class='footer-logo'>
             <Link to='/' className='social-logo'>
-              TRVL
+              WREC | NITJ
               <i class='fab fa-typo3' />
             </Link>
           </div>
-          <small class='website-rights'>WREC|NITJ © 2023</small>
+          <small class='website-rights'>WREC | NITJ © 2023</small>
           <div class='social-icons'>
-            <Link
-              class='social-icon-link facebook'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
-            >
-              <i class='fab fa-facebook-f' />
-            </Link>
             <Link
               class='social-icon-link instagram'
               to='/'
@@ -91,14 +77,6 @@ function Footer() {
               aria-label='Youtube'
             >
               <i class='fab fa-youtube' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
-            >
-              <i class='fab fa-twitter' />
             </Link>
             <Link
               class='social-icon-link twitter'
