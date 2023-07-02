2e54db8317638f6c948edca6283cf5532a6eb50a
Anshika <anshika140203@gmail.com>
2023-07-02 12:31:40 +0530

    committee page updated

--- src/components/pages/Committee.js
+++ src/components/pages/Committee.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import '../../App.css';
+import './committee.css'
 
 export default function Committee() {
-  return <h1 className='committee'>COMMITTEE</h1>;
+  return
+  ( 
+  <><h1 className='committee'>COMMITTEE</h1></>
+  )
 }
+
+
+
+
+
+ 
